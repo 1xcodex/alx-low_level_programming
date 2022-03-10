f1e654b29c8c04f492c03b50d2da85712a38b559
root <you@example.com>
2022-03-10 12:56:31 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#include<stdio.h>
+
+ /**
+  * main - Entry point
+  *
+  * Description : This is a C program that prints a function with Put
+  *
+  * return: value 0
+  */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0);
+
+}
