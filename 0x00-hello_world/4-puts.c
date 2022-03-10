b5499da96fc72686e65114d927eb6c23eb2c3b02
root <you@example.com>
2022-03-10 13:18:49 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,10 @@
   * main - Entry point
   *
   * Description : This is a C program that prints a function
-  * return: value 0
+  * return: value(0)
   */
 int main(void)
-
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return(0);
-
+    return (0);
 }
