12c7a65e9d29701d4c196e572d547d20c11345c6
root <root@DESKTOP-5AMOVAT.localdomain>
2022-04-28 22:30:25 +0300

    bitnew

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
-/*
- * File: main.h
- *
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
+#include <stddef.h>
+#include <stdio.h>
+
+#ifdef DEBUG /*prints debug statements*/
+#endif
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -17,4 +16,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* MAIN_H */
+#endif
